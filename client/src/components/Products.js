69ffea5b8adc85c7eb4e43ec95b28c15e0083f6b
Abhishek <yadavabhishek241296@gmail.com>
2022-07-21 14:20:20 +0530

    API gateway

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,27 +1,37 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import {popularProducts} from '../data'
-import Product from './Product'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
-const Products = ({cat,filter,sort}) => {
-    
-    const [products, setProducts] = useState([])
-    const [filteredProducts, setFilteredProducts] = useState([])
+const Products = ({ cat, filter, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-    return (
-        <Container>
-            {popularProducts.map(item => (
-                <Product item = {item} key = {item.id}/>
-            ))}
-        </Container>
-    )
-}
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/products");
+        console.log(res);
+      } catch (error) {}
+    };
+    getProducts();
+  }, [cat]);
 
-export default Products
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
+
+export default Products;
